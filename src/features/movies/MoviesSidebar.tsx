15ffc18164be118428b7d1949a2a1cf7b543c6f0
Adam Borowski <fl.borovsky@gmail.com>
2022-05-17 16:41:33 +0200

    feat: extract useMovies

--- src/features/movies/MoviesSidebar.tsx
+++ src/features/movies/MoviesSidebar.tsx
@@ -1,69 +1,20 @@
-import { FC, useEffect, useState } from 'react';
-import { MovieLite, searchMovies } from '../../services/movies';
+import { FC } from 'react';
+import { MoviesState, useMovies } from './useMovies';
 
 export interface MoviesSidebarProps {}
 
-type MoviesState =
-  | {
-      type: 'idle';
-    }
-  | {
-      type: 'loading';
-    }
-  | {
-      type: 'success';
-      movies: MovieLite[];
-    }
-  | {
-      type: 'error';
-      message: string;
-    };
-
 export const MoviesSidebar: FC<MoviesSidebarProps> = ({}) => {
-  const [moviesState, setMovieState] = useState<MoviesState>({ type: 'idle' });
+  const { movies } = useMovies();
 
-  useEffect(() => {
-    const abortController = new AbortController();
-
-    (async () => {
-      console.log('use effect !!!!');
-
-      try {
-        setMovieState({ type: 'loading' });
-        const result = await searchMovies(
-          {
-            query: 'police',
-            year: undefined,
-            page: 1,
-          },
-          abortController.signal
-        );
-
-        setMovieState({ type: 'success', movies: result.results });
-      } catch (e) {
-        if (e instanceof DOMException && e.name === 'AbortError') {
-          setMovieState({ type: 'idle' });
-        } else {
-          setMovieState({
-            type: 'error',
-            message: e instanceof Error ? e.message : String(e),
-          });
-        }
-      }
-    })();
-
-    return () => {
-      abortController.abort();
-    };
-  }, []);
-
-  return (
-    <>
-      Tutaj będzie lista filmów
-      {moviesState.type === 'loading' && 'loading'}
-      {moviesState.type === 'error' && <div>Error: {moviesState.message}</div>}
-      {moviesState.type === 'success' &&
-        moviesState.movies.map((movie) => <div>Movie: {movie.title}</div>)}
-    </>
-  );
+  return <MoviesSidebarPure movies={movies} />;
 };
+
+export const MoviesSidebarPure: FC<{ movies: MoviesState }> = ({ movies }) => (
+  <>
+    Tutaj będzie lista filmów
+    {movies.type === 'loading' && 'loading'}
+    {movies.type === 'error' && <div>Error: {movies.message}</div>}
+    {movies.type === 'success' &&
+      movies.movies.map((movie) => <div>Movie: {movie.title}</div>)}
+  </>
+);
---
+++ src/features/movies/README.md
@@ -0,0 +1,4 @@
+* sidebar navigates to /movies/:movieId
+* movie preview next to sidebar
+* movie preview fetches full data
+* movie preview receives movieLite from list for early presence (lifting state up)
